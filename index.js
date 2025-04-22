0b737e3d5d1e489aac11b412fd52dde469470cd8
Hitesh sharma <hitesh444sharma@gmail.com>
2025-04-22 08:03:39 +0530

    updated successfully

---
+++ index.js
@@ -0,0 +1,16 @@
+$('.bxslider').bxSlider({
+    minSlides: 3,
+    maxSlides: 4,
+    slideWidth: 300,
+    slideMargin: 48
+  });
+
+
+
+  $('.bxsliderone').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 880
+  });
